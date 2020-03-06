67a99035f58e463a01a4787dd97a8b4a7d8ef87b
Callista <42157487+callistachi@users.noreply.github.com>
2020-03-06 23:29:19 +1100

    Changed contact + added fade
    Removed obnoxious email header in contact section. + Added fade

---
+++ js/script.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('#about-me', {delay: 300});
+ScrollReveal().reveal('#work-experience', {delay: 300});
+ScrollReveal().reveal('#skills', {delay: 300});
+ScrollReveal().reveal('#languages', {delay: 300});
+ScrollReveal().reveal('#projects', {delay: 300});
+ScrollReveal().reveal('#contact', {delay: 300});
