57e6946f3a688a4bafceeb28681daa6086e3f33a
Callista <42157487+callistachi@users.noreply.github.com>
2020-03-07 01:42:16 +1100

    Added max-width

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ ScrollReveal().reveal('#skills', {delay: 300});
 ScrollReveal().reveal('#languages', {delay: 300});
 ScrollReveal().reveal('#projects', {delay: 300});
 ScrollReveal().reveal('#contact', {delay: 300});
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
