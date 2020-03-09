2131b2f57553006db5b7a5d343b970f736b34578
Callista <42157487+callistachi@users.noreply.github.com>
2020-03-09 14:16:05 +1100

    Added scrollbar styling, partially complete language section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@ ScrollReveal().reveal('#skills', {delay: 300});
 ScrollReveal().reveal('#languages', {delay: 300});
 ScrollReveal().reveal('#projects', {delay: 300});
 ScrollReveal().reveal('#contact', {delay: 300});
+
+$('#circle').circleProgress({
+    value: 0.5,
+    size: 400,
+    thickness: 50,
+    startAngle: -Math.PI/2,
+    fill: {
+        color: '#35E8C6'
+    },
+    emptyFill: 'rgba(36, 18, 41, 1)',
+    animation: {
+      duration: 1500,
+      easing: 'circleProgressEasing'
+    }
+});
