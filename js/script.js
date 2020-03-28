75179bc947fd05deb69a00c446a7249f93fe5c3b
Callista <42157487+callistachi@users.noreply.github.com>
2020-03-28 21:50:02 +1100

    Changed hamburger menu to use JS

--- js/script.js
+++ js/script.js
@@ -1,25 +1,8 @@
 ScrollReveal().reveal('.hero', {delay: 400});
 ScrollReveal().reveal('#about-me', {delay: 300});
 ScrollReveal().reveal('#work-experience', {delay: 300});
-ScrollReveal().reveal('#skills', {delay: 300});
-ScrollReveal().reveal('#languages', {delay: 300});
 ScrollReveal().reveal('#projects', {delay: 300});
 ScrollReveal().reveal('#contact', {delay: 300});
-
-// $('#circle').circleProgress({
-//   value: 0.5,
-//   size: 400,
-//   thickness: 50,
-//   startAngle: -Math.PI/2,
-//   fill: {
-//     color: '#35E8C6'
-//   },
-//   emptyFill: 'rgba(36, 18, 41, 1)',
-//   animation: {
-//     duration: 1500,
-//     easing: 'circleProgressEasing'
-//   }
-// });
 
 const cursor = document.querySelector('.cursor');
 const cursor_outline = document.querySelector('.cursor-outline');
@@ -83,6 +66,13 @@ document.addEventListener('click', () => {
 document.addEventListener('click', () => {
   heart.classList.toggle("heart-active");
 })
+
+$(document).ready(function(){
+  $(".hamburger").click(function(){
+    $(this).toggleClass("is-active");
+    $("#hamburgerMenu").toggleClass("is-active2");
+  });
+});
 
 particlesJS("particles-js", {
   "particles": {
